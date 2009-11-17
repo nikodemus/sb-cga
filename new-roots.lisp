5e21624e8db15cc1f8e86482895594d943cb36d6
Nikodemus Siivola <nikodemus@random-state.net>
2009-11-17 14:29:09 +0200

    bit more work on the new root solvers
     * All three work now.
    
     * Changed all to use double floats: the loss of precision
       for quartics is too atrocious otherwise. Need to think
       about the API a bit: I think I do want to keep the public
       API as single-floats for consistency for now, but using
       doubles internally is a must.

--- new-roots.lisp
+++ new-roots.lisp
@@ -29,6 +29,12 @@
 (sb-alien:define-alien-routine ("cbrtf" cbrt/single) float (float float))
 (sb-alien:define-alien-routine ("cbrt" cbrt/double) sb-alien:double (double sb-alien:double))
 
+(defconstant +eqn-eps+ 1d-9)
+
+(declaim (inline is-zero))
+(defun is-zero (x)
+  (~ 0.0d0 x +eqn-eps+))
+
 (declaim (inline cbrt))
 (defun cbrt (float)
   "Cube root of FLOAT."
@@ -38,38 +44,45 @@
     (double-float
      (cbrt/double float))))
 
-(defun solve-quadric (quadric roots &key (offset 0))
+(declaim (inline make-roots))
+(defun make-roots (n)
+  (declare (fixnum n))
+  (make-array n :element-type 'double-float))
+
+(defun solve-quadric (quadric &optional (roots (make-roots 2))
+                      &key (offset 0))
   "Solve QUADRIC, returning number of real roots and storing their values in ROOTS,
 starting at OFFSET."
-  (declare (type (simple-array single-float (*)) quadric roots))
+  (declare (type (simple-array double-float (*)) quadric roots))
   ;; normal form: x^2 + px + q = 0
   (let* ((K (aref quadric 2))
-         (p (/ (aref quadric 1) (* 2.0 K)))
+         (p (/ (aref quadric 1) (* 2.0d0 K)))
          (q (/ (aref quadric 0) K))
          (D (- (* p p) q)))
-    (cond ((~ 0 D)
-           (setf (aref roots 0) (- p))
-           1)
-          ((< D 0)
-           0)
+    (cond ((is-zero D)
+           (setf (aref roots (+ offset 0)) (- p))
+           (values 1 roots))
+          ((minusp D)
+           (values 0 roots))
           (t
            (let ((sqrt-D (sqrt D)))
              (setf (aref roots (+ offset 0)) (- sqrt-D p)
                    (aref roots (+ offset 1)) (- (- sqrt-D) p))
-             2)))))
+             (values 2 roots))))))
 
-(defun solve-cubic (cubic roots)
+(defun solve-cubic (cubic &optional (roots (make-roots 3)))
   "Solve CUBIC, returning number of real roots and storing their values in ROOTS."
-  (declare (type (simple-array single-float (*)) cubic roots))
+  (declare (type (simple-array double-float (*)) cubic roots))
   ;; Convert to double-floats: we lose too much precision during the solution
   ;; otherwise.
   ;;
   ;; Normal form: x^3 + Ax^2 + Bx + C = 0
-  (let* ((K (/ 1.0d0 (aref cubic 3)))
-         (A (* (aref cubic 2) K))
-         (B (* (aref cubic 1) K))
-         (C (* (aref cubic 0) K))
+  (let* ((K (aref cubic 3))
+         (A (/ (aref cubic 2) K))
+         (B (/ (aref cubic 1) K))
+         (C (/ (aref cubic 0) K))
          (n-roots 0))
+    #+nil
     (when (zerop C)
       (setf (aref roots 0) 0.0)
       (return-from solve-cubic 1))
@@ -80,69 +93,68 @@ starting at OFFSET."
       ;; Use Cardano's formula
       (let* ((p^3 (* p p p))
              (D (+ (* q q) p^3)))
-        (if (~ 0 D)
-            (if (~ 0 q)
+        (if (is-zero D)
+            (if (is-zero q)
                 ;; one triple solution
-                (setf (aref roots 0) 0.0
+                (setf (aref roots 0) 0.0d0
                       n-roots 1)
                 ;; one single and one double solution
                 (let ((u (cbrt (- q))))
-                  (setf (aref roots 0) (coerce (* 2 u) 'single-float)
-                        (aref roots 1) (coerce (- u) 'single-float)
+                  (setf (aref roots 0) (* 2 u)
+                        (aref roots 1) (- u)
                         n-roots 2)))
-            (if (< D 0)
+            (if (minusp D)
                 ;; Casus irreducibilis: three real solutions
                 (let ((phi (* 1/3 (acos (/ (- q) (sqrt (- p^3))))))
                       (th (* 2 (sqrt (- p)))))
-                  (setf (aref roots 0) (coerce (* th (cos phi)) 'single-float)
-                        (aref roots 1) (coerce (* (- th) (cos (+ phi (/ +pi+ 3)))) 'single-float)
-                        (aref roots 2) (coerce (* (- th) (cos (- phi (/ +pi+ 3)))) 'single-float)
+                  (setf (aref roots 0) (* th (cos phi))
+                        (aref roots 1) (* (- th) (cos (+ phi (/ +pi+ 3))))
+                        (aref roots 2) (* (- th) (cos (- phi (/ +pi+ 3))))
                         n-roots 3))
                 ;; One real solution
                 (let* ((sqrt-D (sqrt D))
                        (u (cbrt (- sqrt-D q)))
                        (v (- (cbrt (+ sqrt-D q)))))
-                  (setf (aref roots 0) (coerce (+ u v) 'single-float)
+                  (setf (aref roots 0) (+ u v)
                         n-roots 1))))
         ;; Resubstitute
-        (let ((sub (coerce (* 1/3 A) 'single-float)))
+        (let ((sub (* 1/3 A)))
           (dotimes (i n-roots)
             (decf (aref roots i) sub)))
         ;; All done.
-        n-roots))))
+        (values n-roots roots)))))
 
-(defun solve-quartic (quartic roots)
-  (declare (type (simple-array single-float (5)) quartic)
-           (type (simple-array single-float (4)) roots))
-  (let ((coeffs (make-array 4 :element-type 'single-float)))
+(defun solve-quartic (quartic &optional (roots (make-roots 4)))
+  (declare (type (simple-array double-float (*)) quartic roots))
+  (let ((coeffs (make-array 4 :element-type 'double-float)))
     (declare (dynamic-extent coeffs))
-    (let* ((K (/ 1.0 (aref quartic 4)))
+    (let* ((K (aref quartic 4))
            ;; normal form: x^4 + Ax^3 + Bx^2 + Cx + D = 0
-           (A (* (aref quartic 3) K))
-           (B (* (aref quartic 2) K))
-           (C (* (aref quartic 1) K))
-           (D (* (aref quartic 0) K))
+           (A (/ (aref quartic 3) K))
+           (B (/ (aref quartic 2) K))
+           (C (/ (aref quartic 1) K))
+           (D (/ (aref quartic 0) K))
            ;; substitute x = y - A/4 to eliminate cubic term: x^4 + px^2 + qx + r = 0
            (A^2 (* A A))
            (p (+ (* -3/8 A^2) B))
-           (q (+ (- (* 1/8 A^2) (* 1/2 A B)) C))
+           (q (+ (* 1/8 A^2 A) (* -1/2 A B) C))
            (r (+ (* -3/256 A^2 A^2) (* 1/16 A^2 B) (* -1/4 A C) D))
            (n-roots 0))
-      (cond ((~ 0 r)
+      (cond ((is-zero r)
              ;; no absolute term: y(y^3 + py + q) = 0
              (setf (aref coeffs 0) q
                    (aref coeffs 1) p
-                   (aref coeffs 2) 0.0
-                   (aref coeffs 3) 1.0
+                   (aref coeffs 2) 0.0d0
+                   (aref coeffs 3) 1.0d0
                    n-roots (solve-cubic coeffs roots)
-                   (aref roots n-roots) 0.0)
+                   (aref roots n-roots) 0.0d0)
              (incf n-roots))
             (t
              ;; solve the resolvent cubic ...
-             (setf (aref coeffs 0) (+ (* 1/2 r p) (* -1/8 q q))
+             (setf (aref coeffs 0) (- (* 1/2 r p) (* 1/8 q q))
                    (aref coeffs 1) (- r)
                    (aref coeffs 2) (* -1/2 p)
-                   (aref coeffs 3) 1.0)
+                   (aref coeffs 3) 1.0d0)
              (solve-cubic coeffs roots)
              ;; ... and take the one real solution ...
              (let ((z (aref roots 0)))
@@ -150,77 +162,97 @@ starting at OFFSET."
                (let ((u (- (* z z) r))
                      (v (- (* 2 z) p)))
                  (cond ((~ 0 u)
-                        (setf u 0.0))
+                        (setf u 0.0d0))
                        ((> u 0)
                         (setf u (sqrt u)))
                        (t
-                        (return-from solve-quartic 0)))
+                        (return-from solve-quartic (values 0 roots))))
                  (cond ((~ 0 v)
-                        (setf v 0.0))
+                        (setf v 0.0d0))
                        ((> v 0)
                         (setf v (sqrt v)))
                        (t
-                        (return-from solve-quartic 0)))
+                        (return-from solve-quartic (values 0 roots))))
                  (setf (aref coeffs 0) (- z u)
                        (aref coeffs 1) (if (< q 0) (- v) v)
-                       (aref coeffs 2) 1.0)
+                       (aref coeffs 2) 1.0d0)
                  (setf n-roots (solve-quadric coeffs roots))
                  (setf (aref coeffs 0) (+ z u)
                        (aref coeffs 1) (if (< q 0) v (- v))
-                       (aref coeffs 2) 1.0)
+                       (aref coeffs 2) 1.0d0)
                  (incf n-roots (solve-quadric coeffs roots :offset n-roots))))))
       ;; resubstitute
       (let ((sub (* 1/4 A)))
         (dotimes (i n-roots)
           (decf (aref roots i) sub))
         ;; All done!
-        n-roots))))
+        (values n-roots roots)))))
 
 ;;; Tests
 
-(defun test-quadratic ()
-  (let ((n 0))
-    (loop for a from -20.0 upto 20.0 by 1.0
-          do (unless (zerop a)
-               (loop for b from -20.0 upto 20.0 by 1.0
-                     do (loop for c from -20.0 upto 20.0 by 1.0
-                              do (let* ((quad (make-array 3 :element-type 'single-float
-                                                          :initial-contents (list c b a)))
-                                        (sol (make-array 2 :element-type 'single-float))
-                                        (roots (sb-cga::solve-quadric quad sol)))
-                                   (incf n roots)
-                                   (dotimes (i roots)
-                                     (let* ((x (aref sol i))
-                                            (y (+ (* a x x) (* b x) c)))
-                                       (unless (sb-cga::~ 0 y 0.0001)
-                                         (error "~S, ~S, ~S: ~S, ~S => ~S"
-                                                a b c
-                                                (subseq sol 0 roots)
-                                                x y)))))))))
-    n))
+(defun equation (&rest coeffs)
+  (make-array (length coeffs)
+              :element-type 'double-float
+              :initial-contents (mapcar (lambda (c) (coerce c 'double-float))
+                                        (reverse coeffs))))
 
+(defun map-coefficients (fun n start stop &optional (step 1))
+  (labels ((rec (n coeffs)
+             (if (zerop n)
+                 ;; No degenerate cases.
+                 (unless (zerop (car coeffs))
+                   (apply fun coeffs))
+                 (loop for c from start upto stop by step
+                       do (rec (1- n) (cons c coeffs))))))
+    (rec n nil)))
+
+(defun test-quadric ()
+  (let ((n 0))
+    (map-coefficients
+     (lambda (a b c)
+       (multiple-value-bind (n-roots roots) (solve-quadric (equation a b c))
+         (incf n n-roots)
+         (dotimes (i n-roots)
+           (let* ((x (aref roots i))
+                  (res (+ (* a x x) (* b x) c)))
+             (unless (~ 0.0 res 0.0001)
+               (error "~S, ~S, ~S: ~S, ~S => ~S"
+                      a b c
+                      (subseq roots 0 n-roots)
+                      x res))))))
+     3 -20 20)
+    n))
 
 (defun test-cubic ()
   (let ((n 0))
-    (loop for a from -10.0 upto 10.0 by 1.0
-          do (unless (zerop a)
-               (loop for b from -10.0 upto 10.0 by 1.0
-                     do (loop for c from -10.0 upto 10.0 by 1.0
-                              do (loop for d from -10.0 upto 10.0 by 1.0
-                                       do (incf n (test-1-cubic a b c d)))))))
+    (map-coefficients
+     (lambda (a b c d)
+       (multiple-value-bind (n-roots roots) (solve-cubic (equation a b c d))
+         (incf n n-roots)
+         (dotimes (i n-roots)
+           (let* ((x (aref roots i))
+                  (res (+ (* a (expt x 3)) (* b (expt x 2)) (* c x) d)))
+             (unless (~ 0 res 0.0001)
+               (error "(test-1-cubic ~S ~S ~S ~S) => ~S : ~S, ~S"
+                      a b c d
+                      (subseq roots 0 n-roots)
+                      x res))))))
+     4 -10 10 1)
     n))
 
-(defun test-1-cubic (a b c d)
-  (let* ((cubic (make-array 4 :element-type 'single-float
-                            :initial-contents (list d c b a)))
-         (sol (make-array 3 :element-type 'single-float))
-         (roots (sb-cga::solve-cubic cubic sol)))
-    (dotimes (i roots)
-      (let* ((x (aref sol i))
-             (y (+ (* a x x x) (* b x x) (* c x) d)))
-        (unless (sb-cga::~ 0 y 0.00111)
-          (error "(test-1-cubic ~S ~S ~S ~S) => ~S : ~S, ~S"
-                 a b c d
-                 (subseq sol 0 roots)
-                 x y))))
-    roots))
+(defun test-quartic ()
+  (let ((n 0))
+    (map-coefficients
+     (lambda (a b c d e)
+       (multiple-value-bind (n-roots roots) (solve-quartic (equation a b c d e))
+         (incf n n-roots)
+         (dotimes (i n-roots)
+           (let* ((x (aref roots i))
+                  (res (+ (* a (expt x 4)) (* b (expt x 3)) (* c (expt x 2)) (* d x) e)))
+             (unless (~ 0 res 0.009)
+               (error "quartic: ~S ~S ~S ~S ~S ~S~%~S roots: ~S~%~S => ~S"
+                      a b c d e (list (/ b a) (/ c a) (/ d a) (/ e a))
+                      n-roots roots
+                      x res))))))
+     5 -10.0 10.0 1.0)
+    n))
