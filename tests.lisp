6ed09f02b22fef7ffb59fa25dd2f00eea8282add
Bart Botta <00003b@gmail.com>
2010-12-19 20:35:53 -0600

    don't try to REQUIRE rt on acl or clisp
    need to load by hand for now, probably should replace with ASDF?

--- tests.lisp
+++ tests.lisp
@@ -16,6 +16,7 @@
 ;;;; TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 ;;;; SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+#- (or allegro clisp)
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (require :rt))
 
