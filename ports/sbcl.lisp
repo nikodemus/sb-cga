nikodemus/sb-cga

commit 8d83e4a045f6aa408c82b26dcec5b196849ec1bf

replace sb-vm::make-ea with sb-vm::ea
SBCL had sb-vm::make-ea removed in sbcl-2.0.11-13-g0a3aad22d

ports/sbcl.lisp
@@ -90,17 +90,17 @@
 #+sb-cga-sse2
 (progn
   (defmacro ea-for-data (vector index)
-    `(sb-vm::make-ea :dword :base ,vector
-                     :disp (- (+ (* sb-vm:vector-data-offset sb-vm:n-word-bytes)
-                                 ;; 4 bytes per single-float
-                                 (* ,index 4))
-                              sb-vm:other-pointer-lowtag)))
+    `(sb-vm::ea (- (+ (* sb-vm:vector-data-offset sb-vm:n-word-bytes)
+                      ;; 4 bytes per single-float
+                      (* ,index 4))
+                   sb-vm:other-pointer-lowtag)
+                ,vector))
   (defmacro ea-for-slice (vector &optional (index 0))
-    `(sb-vm::make-ea :dword :base ,vector
-                     :disp (- (+ (* sb-vm:vector-data-offset sb-vm:n-word-bytes)
-                                 ;; 4 bytes per single-float, 16 per slice.
-                                 (* ,index 16))
-                              sb-vm:other-pointer-lowtag)))
+    `(sb-vm::ea (- (+ (* sb-vm:vector-data-offset sb-vm:n-word-bytes)
+                      ;; 4 bytes per single-float, 16 per slice.
+                      (* ,index 16))
+                   sb-vm:other-pointer-lowtag)
+                ,vector))
   (defmacro load-slice (xmm vector &optional (index 0))
     `(inst movaps ,xmm (ea-for-slice ,vector ,index)))
   (defmacro store-slice (xmm vector &optional (index 0))

commit d86110c49b0864703b3890eac324ae2dbe527471

Fix so SBCL versions before 1.4.11 continue working
Removal of make-ea use in commit 8d83e4a broke old SBCL versions.
Thanks to gregcman for pointing it out.

ea was added in 1.4.11
make-ea was removed after 2.0.11
Chose to use make-ea before SBCL 2.0.0

ports/sbcl.lisp
@@ -88,19 +88,46 @@
 ;;;; Utilities for writing VOPs
 
 #+sb-cga-sse2
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  ;; check if we should use sb-vm::ea or make-ea
+  ;; ea was added in 1.4.11
+  ;; make-ea was removed after 2.0.11
+  ;; use make-ea before SBCL 2.0.0
+  (when (< (parse-integer (lisp-implementation-version) :junk-allowed t) 2)
+    (pushnew :sb-cga-make-ea *features*)))
+
+#+sb-cga-sse2
 (progn
+  #-sb-cga-make-ea
   (defmacro ea-for-data (vector index)
     `(sb-vm::ea (- (+ (* sb-vm:vector-data-offset sb-vm:n-word-bytes)
                       ;; 4 bytes per single-float
                       (* ,index 4))
                    sb-vm:other-pointer-lowtag)
                 ,vector))
+  #+sb-cga-make-ea
+  (defmacro ea-for-data (vector index)
+    `(sb-vm::make-ea :dword
+                     :base ,vector
+                     :disp (- (+ (* sb-vm:vector-data-offset sb-vm:n-word-bytes)
+                                 ;; 4 bytes per single-float
+                                 (* ,index 4))
+                              sb-vm:other-pointer-lowtag)))
+  #-sb-cga-make-ea
   (defmacro ea-for-slice (vector &optional (index 0))
     `(sb-vm::ea (- (+ (* sb-vm:vector-data-offset sb-vm:n-word-bytes)
                       ;; 4 bytes per single-float, 16 per slice.
                       (* ,index 16))
                    sb-vm:other-pointer-lowtag)
                 ,vector))
+  #+sb-cga-make-ea
+  (defmacro ea-for-slice (vector &optional (index 0))
+    `(sb-vm::make-ea :dword
+                     :base ,vector
+                     :disp (- (+ (* sb-vm:vector-data-offset sb-vm:n-word-bytes)
+                                 ;; 4 bytes per single-float, 16 per slice.
+                                 (* ,index 16))
+                              sb-vm:other-pointer-lowtag)))
   (defmacro load-slice (xmm vector &optional (index 0))
     `(inst movaps ,xmm (ea-for-slice ,vector ,index)))
   (defmacro store-slice (xmm vector &optional (index 0))